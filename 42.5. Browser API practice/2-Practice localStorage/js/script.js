f6e3933d60caf32c1123d22ddf22abdeb2576604
Tahsin000 <59111015+Tahsin000@users.noreply.github.com>
2023-03-11 18:58:16 +0600

    counter - 11-03-2023

--- 42.5. Browser API practice/2-Practice localStorage/js/script.js
+++ 42.5. Browser API practice/2-Practice localStorage/js/script.js
@@ -1,35 +1,25 @@
-const clearAll = ()=>{
-    localStorage.clear();
-}
 const getElementId = (id)=>{
     return document.getElementById(id);
 }
-
-const singleLocalStorageSave = ()=>{
-    const key = getElementId('userInputKay').value;
-    const value = getElementId('userInputValue').value;
-    localStorage.setItem(key, value);
-    getElementId('userInputKay').value = '';
-    getElementId('userInputValue').value = '';
+const clickME = () =>{
+    const clickCounter = getElementId('clickCounter');
+    const counter = parseInt(clickCounter.innerText) + 1;
+    clickCounter.innerText = counter;
+    SaveLocalStorage(counter);
 }
-const singleLocalStorageGet = ()=>{
-    const SelectId = getElementId('ShowItems');
-    const SearchItem = getElementId('userInputSearch').value;
-    SelectId.innerText = localStorage.getItem(SearchItem);
-    getElementId('userInputSearch').value = '';
+const SaveLocalStorage = (data)=>{
+    localStorage.setItem('counter',data);
 }
-const RemoveItemsWithKey= ()=>{
-    const searchItem = getElementId('removeSearchItem').value;
-    const check = localStorage.getItem(searchItem)
-    localStorage.removeItem(searchItem);
-    const notifi = getElementId('warningText');
-    notifi.classList.remove('alert-success', 'alert-warning');
-    if (check){
-        notifi.classList.add('alert','alert-success');
-        notifi.innerText = 'successfully removed!!!';
+const refreshPage = ()=>{
+    const data = localStorage.getItem('counter');
+    if (data){
+        getElementId('clickCounter').innerText = data;
     } else {
-        notifi.classList.add('alert','alert-warning');
-        notifi.innerText = 'Not Found 404';
+        getElementId('clickCounter').innerText = 0;
     }
-    getElementId('removeSearchItem').value = '';
 }
+const ClearAll =()=>{
+    getElementId('clickCounter').innerText = 0;
+    localStorage.clear();
+}
+refreshPage();
