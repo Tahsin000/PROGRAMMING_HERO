cd2f7ef298b8bdd88c99c3b0f18482ccc8e3db2d
Tahsin000 <59111015+Tahsin000@users.noreply.github.com>
2023-05-01 20:00:48 +0600

    01-05-2023

--- 64.5 Firebase auth and Simple Site Practice/travel-guru-resources/src/component/Card/Card.jsx
+++ 64.5 Firebase auth and Simple Site Practice/travel-guru-resources/src/component/Card/Card.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Card.css'
+import { Link } from 'react-router-dom';
 
-const Card = ({location}) => {
+const Card = ({location, setLocationInfo}) => {
     const locationName = location.location;
-    // const locationPicture = '../../../public/images/Sreemongol.png';
+    const locationId = location._id;
     const pathVariable =  location.img;
     const BgImgPath = `linear-gradient(0.12deg, #000000 0.1%, rgba(0, 0, 0, 0) 69.96%), url(${pathVariable})`
+    const [FindLocation, setFindLocation] = useState();
     const myStyle = {
         "background":BgImgPath,
         "height": "30rem",
@@ -14,13 +16,24 @@ const Card = ({location}) => {
         "fontFamily": "'Bebas Neue', cursive",
         "fontSize":"2.25rem",
     }
+
+    useEffect(()=>{
+        fetch(`http://localhost:5000/locations`)
+        .then(res=>res.json())
+        .then(data=>setFindLocation(data))
+    }, []);
+
+    const handleFindId = ()=>{
+        const FindLocationInfo = FindLocation.find(Lid => Lid._id === locationId);
+        // if (FindLocationInfo)
+            setLocationInfo(FindLocationInfo);
+        // console.log(FindLocation[0])
+    }
     return (
-        
         <div className='p-[0.5rem] drop-shadow-xl'>
-            <div style={myStyle} className="flex items-end rounded-[1.25rem] p-[1.25rem] hover:outline-4 hover:outline-[rgb(251,188,4)] outline outline-[rgba(251,189,4,0)] text-white">
-                <img src={locationName} alt="" />
+            <Link onClick={handleFindId}  style={myStyle} className="flex items-end rounded-[1.25rem] p-[1.25rem] hover:outline-4 hover:outline-[rgb(251,188,4)] outline outline-[rgba(251,189,4,0)] text-white">
                 <p>{locationName}</p>
-            </div>
+            </Link>
         </div>
     );
 };
--- 64.5 Firebase auth and Simple Site Practice/travel-guru-resources/src/component/Home/Home.jsx
+++ 64.5 Firebase auth and Simple Site Practice/travel-guru-resources/src/component/Home/Home.jsx
@@ -13,12 +13,16 @@ import { FaAngleRight } from "react-icons/fa";
 const Home = () => {
 
     const [locations, setLocaions] = useState([]);
+    const [locationInfo, setLocationInfo] = useState([]);
+
     useEffect(()=>{
         fetch('http://localhost:5000/locations')
         .then(res=>res.json())
         .then(data=>setLocaions(data))
     } ,[]);
 
+    
+    console.log(locationInfo);
     const myStyle = {
         "fontFamily" :"'Bebas Neue', cursive",
     }
@@ -43,9 +47,9 @@ const Home = () => {
                 <div className="h-full flex items-center text-white">
                     <div className="grid md:grid-cols-3 gap-5 h-[60%]">
                         <section className='flex flex-col justify-center'>
-                            <h2 className="text-[6rem]" style={{fontFamily :'Bebas Neue'}}>{`Cox's bazar`}</h2>
+                            <h2 className="text-[6rem]" style={{fontFamily :'Bebas Neue'}}>{locationInfo.location || "Cox's Bazar"}</h2>
                             <p className='text-[1rem] font-[400]' style={{fontFamily: 'Montserrat'}}>
-                                {`Cox's Bazar is a city, fishing port, tourism centre and district headquarters in southeastern Bangladesh. It is famous mostly for its long natural sandy beach, and it ...`}
+                                {locationInfo.description || "Cox's Bazar is a popular beach town on the Bay of Bengal, known for its long sandy beach, clear blue waters, and stunning sunsets."} 
                             </p>
                             <Link to='/booking' className='flex justify-center text-black gap-3 items-center p-5 bg-[#F9A51A] w-[50%] rounded-lg mt-5'>
                                 <span className='text-[1rem] leading-1.25 font-[500]'>Booking</span> <FaAngleRight className='text-[1.25rem] mt-1'></FaAngleRight>
@@ -59,12 +63,8 @@ const Home = () => {
                                 onSwiper={(swiper) => console.log(swiper)}
                             >
                                 {
-                                    locations.map(location => <SwiperSlide key={location._id} className="h-full" > <Card location={location}> </Card> </SwiperSlide>)
+                                    locations.map(location => <SwiperSlide key={location._id} className="h-full" > <Card location={location} setLocationInfo={setLocationInfo}> </Card> </SwiperSlide>)
                                 }
-                                
-                                {/* <SwiperSlide className="h-full " > <Card> </Card> </SwiperSlide>
-                                <SwiperSlide className="h-full " > <Card> </Card> </SwiperSlide>
-                                <SwiperSlide className="h-full " > <Card> </Card> </SwiperSlide> */}
                             </Swiper>
                         </section>
                     </div>
